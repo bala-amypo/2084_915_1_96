377b3f5d93dfbd3cda070990f0529ced6872ec1a
bala-amypo <bala@amypo.in>
2025-12-27 04:30:55 +0000

    User commit

--- src/main/java/com/example/demo/repository/CapacityAlertRepository.java
+++ src/main/java/com/example/demo/repository/CapacityAlertRepository.java
@@ -2,10 +2,12 @@ package com.example.demo.repository;
 
 import com.example.demo.model.CapacityAlert;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Repository
 public interface CapacityAlertRepository extends JpaRepository<CapacityAlert, Long> {
-    List<CapacityAlert> findByTeamNameAndDate(String teamName, LocalDate date);
+    List<CapacityAlert> findByTeamNameAndDateBetween(String teamName, LocalDate start, LocalDate end);
 }
